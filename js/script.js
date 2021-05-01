b830bce2334bbeeee3c90d225677d16cab17e68a
vuducminh <vuducminhkyle@gmail.com>
2021-05-01 10:44:48 +0700

    add button gridmap

--- js/script.js
+++ js/script.js
@@ -16,13 +16,6 @@ Grid.prototype.getBounds = function () {
     return this.rectangleBounds(0, 0, 1);
 };
 Grid.prototype.rectangleBounds = function (x, y, scale) {
-    // var yStep = this.radius * 2 / scale;
-    // var xStep = yStep;
-    // var north = uiSettings.LatOrigin - this.radius + yStep * x;
-    // var south = uiSettings.LatOrigin - this.radius + yStep * (x + 1);
-    // var west = uiSettings.LngOrigin - this.radius * 2 + xStep * (y + 1);
-    // var east = uiSettings.LngOrigin - this.radius * 2 + xStep * y;
-
     var north = uiSettings.LatOrigin + uiSettings.size * (x + 1);
     var south = uiSettings.LatOrigin + uiSettings.size * x;
     var east = uiSettings.LngOrigin + uiSettings.size * y;
@@ -55,14 +48,9 @@ function GridOverlay(map) {
 }
 
 GridOverlay.prototype.computeRadius = function () {
-    // return (this.map.getBounds().getNorthEast().lat() - this.map.getBounds().getSouthWest().lat()) * uiSettings.gridToViewportLeastDiameterRatio / 2
     return (105.9 - 105.765) /2
 };
 GridOverlay.prototype.redraw = function () {
-    this.elements.forEach(function (element) {
-        element.setMap(null);
-    });
-    this.elements = [];
     var grid = this.currentGrid();
     this.elements = this.createElements(grid);
     this.elements.forEach(function (element) {
@@ -93,6 +81,53 @@ GridOverlay.prototype.createElements = function (grid) {
 GridOverlay.prototype.init = function () {
 
 };
+function CenterControl(controlDiv, map) {
+    // Set CSS for the control border.
+    var controlUI = document.createElement("div");
+    controlUI.style.backgroundColor = "#3498db";
+    controlUI.style.border = "2px solid #3498db";
+    controlUI.style.borderRadius = "3px";
+    controlUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
+    controlUI.style.cursor = "pointer";
+    controlUI.style.marginTop = "8px";
+    controlUI.style.marginBottom = "22px";
+    controlUI.style.textAlign = "center";
+    controlUI.title = "Click to recenter the map";
+    controlDiv.appendChild(controlUI);
+    // Set CSS for the control interior.
+    var controlText = document.createElement("div");
+    controlText.style.color = "rgb(255,255,255)";
+    controlText.style.fontFamily = "Roboto,Arial,sans-serif";
+    controlText.style.fontSize = "16px";
+    controlText.style.lineHeight = "38px";
+    controlText.style.paddingLeft = "5px";
+    controlText.style.paddingRight = "5px";
+    controlText.innerHTML = "Grid Map";
+    controlUI.appendChild(controlText);
+    // Setup the click event listeners: simply set the map to Chicago.
+    controlUI.addEventListener("click", () => {
+        gridOverlay.clickGridMap(controlUI,controlText);
+    });
+}
+
+GridOverlay.prototype.clickGridMap = function (controlUI,controlText) {
+    if(this.elements == null) {
+        this.redraw();
+        controlUI.style.backgroundColor = "#3498db";
+        controlUI.style.border = "2px solid #3498db";
+        controlText.style.color = "rgb(255,255,255)";
+    }
+    else {
+        this.elements.forEach(function (element) {
+            element.setMap(null);
+        });
+        this.elements = null;
+        controlUI.style.backgroundColor = "#fff";
+        controlUI.style.border = "2px solid #fff";
+        controlText.style.color = "rgb(25,25,25)";
+    }
+};
+
 
 var map;
 var gridOverlay;
@@ -120,6 +155,9 @@ function initMap() {
         // map.addListener('center_changed', function () {
         //     gridOverlay.redraw();
         // });
+        const centerControlDiv = document.createElement("div");
+        CenterControl(centerControlDiv, map);
+        map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
         gridOverlay = new GridOverlay(map);
         gridOverlay.init();
         gridOverlay.redraw();
